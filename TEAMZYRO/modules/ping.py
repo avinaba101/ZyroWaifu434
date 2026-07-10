fbf12b0f936363cebaaf56d548d0b19b232462a4
avinaba101 <sarkaravinaba558@gmail.com>
2026-07-10 16:29:58 +0530

    Update ping.py

--- TEAMZYRO/modules/ping.py
+++ TEAMZYRO/modules/ping.py
@@ -5,20 +5,24 @@
 # ==========================================
 
 import time
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
-from telegram import Update
-from telegram.ext import CommandHandler, CallbackContext
+from TEAMZYRO import app, is_vip_or_owner
 
-from TEAMZYRO import application, is_vip_or_owner
-
-async def ping(update: Update, context: CallbackContext) -> None:
-    if not await is_vip_or_owner(update.effective_user.id):
-        await update.message.reply_text("Nouu.. only Owner or VIP users can run this command..")
+@app.on_message(filters.command("ping"))
+async def ping(client: Client, message: Message):
+    user_id = message.from_user.id if message.from_user else None
+    if not user_id:
         return
+        
+    if not await is_vip_or_owner(user_id):
+        await message.reply_text("Nouu.. only Owner or VIP users can run this command..")
+        return
+        
     start_time = time.time()
-    message = await update.message.reply_text('Pong!')
+    reply_msg = await message.reply_text('Pong!')
     end_time = time.time()
+    
     elapsed_time = round((end_time - start_time) * 1000, 3)
-    await message.edit_text(f'Pong! {elapsed_time}ms')
-
-application.add_handler(CommandHandler("ping", ping))
+    await reply_msg.edit_text(f'Pong! {elapsed_time}ms')
